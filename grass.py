48043c2471e967800d87f3dd0c33491659c9c513
Armando Partida Sanabia <armandopartida0@gmail.com>
2019-10-19 15:03:40 -0400

    Add grass script

---
+++ grass.py
@@ -0,0 +1,14 @@
+import pygame
+from settings import *
+
+
+class Grass(pygame.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites
+        pygame.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = pygame.image.load('assets/images/tiles/grass.png')
+        self.image = pygame.transform.rotozoom(self.image, 0, 2)
+        self.rect = self.image.get_rect()
+        self.rect.x = x * TILESIZE
+        self.rect.y = y * TILESIZE
