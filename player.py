c8ff1c5df43252e10f8ec22264866ca23733a42f
Armando Partida Sanabia <armandopartida0@gmail.com>
2019-10-19 17:19:42 -0400

    Use new character

--- player.py
+++ player.py
@@ -14,11 +14,26 @@ class Player(pygame.sprite.Sprite):
         self.size = 64
 
         # image
+        self.stand_back = pygame.image.load('assets/images/Garyinstandback.png')
+        self.stand_back = pygame.transform.rotozoom(self.stand_back, 0, 2)
+        self.stand_back = pygame.transform.rotozoom(self.stand_back, 0, 0.75)
+
+        self.stand_front = pygame.image.load('assets/images/Garyinstandfront.png')
+        self.stand_front = pygame.transform.rotozoom(self.stand_front, 0, 2)
+        self.stand_front = pygame.transform.rotozoom(self.stand_front, 0, 0.75)
+
+        self.stand_right = pygame.image.load('assets/images/stand_right.png')
+        self.stand_right = pygame.transform.rotozoom(self.stand_right, 0, 2)
+        self.stand_right = pygame.transform.rotozoom(self.stand_right, 0, 0.75)
+
+        self.stand_left = pygame.image.load('assets/images/stand_right.png')
+        self.stand_left = pygame.transform.flip(self.stand_left, 1, 0)
+        self.stand_left = pygame.transform.rotozoom(self.stand_left, 0, 2)
+        self.stand_left = pygame.transform.rotozoom(self.stand_left, 0, 0.75)
 
         self.pressed_keys = []
 
-        self.image = pygame.image.load('assets/images/playerBig.png')
-        self.image = pygame.transform.rotozoom(self.image, 0, 0.5)
+        self.image = self.stand_front
         self.rect = self.image.get_rect()
         self.rect.x = x * settings.TILESIZE
         self.rect.y = y * settings.TILESIZE
@@ -27,15 +42,19 @@ class Player(pygame.sprite.Sprite):
         if self.pressed_keys[pygame.K_w]:
             self.rect.y -= self.speed
             self.direction = 'U'
+            self.image = self.stand_back
         elif self.pressed_keys[pygame.K_s]:
             self.rect.y += self.speed
             self.direction = 'D'
+            self.image = self.stand_front
         elif self.pressed_keys[pygame.K_a]:
             self.rect.x -= self.speed
             self.direction = 'L'
+            self.image = self.stand_left
         elif self.pressed_keys[pygame.K_d]:
             self.rect.x += self.speed
             self.direction = 'R'
+            self.image = self.stand_right
 
     def load_keys(self, keys):
         self.pressed_keys = keys
