292537892afb7752d70099087ffc04181cffd542
GVU-GAME <gvu@clemson.edu>
2019-10-19 10:00:18 -0400

    fix main
    fix?

--- main.py
+++ main.py
@@ -2,17 +2,28 @@ import pygame
 import util
 
 # Initialization
-pygame.init()
-screen = pygame.display.set_mode((util.SCREEN_WIDTH, util.SCREEN_HEIGHT))
-pygame.display.set_caption(util.PROJECT_NAME)
-running = True
+class Game:
+    def __init__(self):
+        self.sfx =
+        pygame.init()
+        pygame.mixer.init()
+        self.screen = pygame.display.set_mode((util.SCREEN_WIDTH, util.SCREEN_HEIGHT))
+        pygame.display.set_caption(util.PROJECT_NAME)
+
+    def load_data(self):
+        pass
+
+    def new(self):
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
+
+    def events(self):
+        pass
+
 
 # Game loop
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    # Update
-
-    # Draw
